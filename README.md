05569fd8236ba1682ee1ab31143e17a46c43ffa0
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-04-17 13:39:27 +0530

    version 1.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# QR-Code-Generator-And-Reader
+This is a simple QR code generator and reader made by using pyqrcode library in python
---
+++ qr_generator_reader.py
@@ -0,0 +1,26 @@
+import pyqrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+while True:
+    choice = int(input("Enter 1 to generate QR code and 2 to read QR code: "))
+    if choice == 1:
+        text = input("Enter the text you want to convert into QR code: ")
+
+        qr = pyqrcode.create(text)
+        qr.png("myCode.png", scale=8)
+
+    elif choice == 2:
+        img = input(
+            "Enter the name of the image that contains the QR code, please enter the correct name in case sensitive: ")
+
+        d = decode(Image.open(f"{img}.png"))
+        print(d[0].data.decode("ascii"))
+
+    else:
+        print("You entered something else.\nTry Again... ")
+
+    loop_out = input("Enter q to quit this loop: ")
+
+    if loop_out == 'q':
+        break
