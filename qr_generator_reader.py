9429ca6dac4a76444e403d862e0fcc54fa005ac9
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-04-19 15:32:38 +0530

    version 1.1 updating GUI

--- qr_generator_reader.py
+++ qr_generator_reader.py
@@ -1,6 +1,7 @@
 import pyqrcode
 from pyzbar.pyzbar import decode
 from PIL import Image
+from tkinter.filedialog import *
 
 while True:
     choice = int(input("Enter 1 to generate QR code and 2 to read QR code: "))
@@ -11,10 +12,10 @@ while True:
         qr.png("myCode.png", scale=8)
 
     elif choice == 2:
-        img = input(
-            "Enter the name of the image that contains the QR code, please enter the correct name in case sensitive: ")
+        # img = input("Enter the name of the image that contains the QR code, please enter the correct name in case sensitive: ")
+        img = askopenfilename()
 
-        d = decode(Image.open(f"{img}.png"))
+        d = decode(Image.open(f"{img}"))
         print(d[0].data.decode("ascii"))
 
     else:
